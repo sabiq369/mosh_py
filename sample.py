b075eb8ab30970193fd0b8e2ba3e6323236492c8
sabiq mohammed <sabiq.mohammed4@gmail.com>
2022-01-08 14:21:22 +0530

    1st commit

---
+++ sample.py
@@ -0,0 +1,45 @@
+# name = "john smith"
+# age = 20
+# is_new = True
+# print(name,age,is_new)
+
+# ---------------------------------------------------------------------------------------------------
+
+
+# name = input("Enter your name : ")
+# color = input("Your favourite color : ")
+# print("Hi " + name +" Your favourite color is " + color)
+
+
+# ---------------------------------------------------------------------------------------------------
+
+
+# weight = float(input("Enter your weight in pounds : "))
+# kg = weight * 0.453592
+# print("You weigh " + str(kg)+ " kg")
+
+
+# ---------------------------------------------------------------------------------------------------
+
+
+
+# name = "sabiq mohammed"
+# print(name.replace('mohammed',"the great"))
+
+
+# ---------------------------------------------------------------------------------------------------
+
+
+# house = 1000000
+# good_credit = False
+# bad_credit = True
+# if good_credit :
+#     print('You are getting  a ' + str("10% ") + "off")
+#     print(house * 10/3)
+# else :
+#     print("You need to put down " + str('20%') )
+#     print(house * 20/3)
+#     print("")
+
+
+
